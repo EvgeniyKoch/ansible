1c651ffddb9544e047e096b04a6196219ec0fec9
EvgeniyKoch <evgkoch@list.ru>
2023-08-15 15:29:28 +0300

    collections

---
+++ collections/sql/003_topics.sql
@@ -0,0 +1,79 @@
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('fugit', '2019-06-03 02:17:29.058', 1, 'placeat maiores aliquam', 39);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Quod quasi molestiae. Aspernatur quod repudiandae quis dolorem. Veritatis nobis explicabo qui qui eum molestias assumenda.', '2019-06-02 23:42:30.688', 2, 'molestiae voluptas velit', 2);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('ducimus', '2019-06-02 18:44:09.440', 3, 'est omnis totam', 98);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('nulla', '2019-06-02 15:51:30.553', 4, 'iste ea temporibus', 21);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Et in sint doloribus recusandae ducimus doloribus omnis consequatur.', '2019-06-03 07:27:48.457', 5, 'expedita id alias', 83);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('facilis a cupiditate', '2019-06-02 17:21:11.462', 6, 'voluptatibus aspernatur laboriosam', 27);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('quisquam voluptas quia', '2019-06-03 08:21:25.020', 7, 'quia mollitia explicabo', 26);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Inventore minima totam ut eum id. Possimus ad quisquam blanditiis est distinctio tempora minus.', '2019-06-02 20:43:13.581', 8, 'est eaque quia', 27);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('nihil', '2019-06-03 06:13:06.906', 9, 'error autem dolores', 94);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Voluptatem natus laudantium fugit explicabo.', '2019-06-03 02:13:00.904', 10, 'a voluptatibus et', 15);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Consequatur velit praesentium culpa eum nam dolore laboriosam. Quas et repellendus. Expedita et aliquam cumque nihil repudiandae assumenda modi ut quisquam. Ut harum cum inventore nostrum neque. Quisquam qui quis eos.', '2019-06-03 03:33:32.462', 11, 'ducimus ullam commodi', 19);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('In accusantium et omnis. Aut voluptates nihil soluta consequatur itaque temporibus debitis neque.', '2019-06-02 14:21:20.964', 12, 'non doloremque eos', 16);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Ducimus exercitationem nihil illum mollitia doloribus sed fuga eum. Modi doloribus voluptatem soluta blanditiis illo aut voluptas architecto iusto. Et distinctio vitae mollitia mollitia laborum.
+
+In a molestiae qui qui temporibus eaque placeat. Nulla distinctio accusantium. Sequi eius a. Et tenetur et laudantium dolorum vel iure rerum. Labore veniam dignissimos non distinctio fuga facilis numquam nostrum officia. Enim et officiis.
+
+Sunt ducimus incidunt est. Consequuntur molestiae vel nobis eos. Illum esse accusantium vero rerum dolore est. Qui cum numquam similique aperiam recusandae sunt. Architecto reprehenderit voluptate est quasi tempora.', '2019-06-02 14:57:52.435', 13, 'eaque sit quo', 48);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Inventore ad suscipit sint modi autem ad quis et dolorem. Ratione voluptatem ullam praesentium molestiae neque quo fuga vel et. Dolorem qui similique dolor ullam est.', '2019-06-02 21:19:41.088', 14, 'eius quidem dicta', 49);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('repellendus', '2019-06-02 21:00:25.439', 15, 'voluptatum aliquid aut', 28);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('laboriosam', '2019-06-03 04:07:36.464', 16, 'placeat eos quis', 97);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Delectus exercitationem a perferendis repellendus explicabo est.
+Facere assumenda voluptatibus deleniti et ab.
+Et expedita tempore tempore dolor optio.
+Ea id cumque dolore suscipit voluptas et.
+Dicta qui quam ut optio quam velit.', '2019-06-02 22:52:23.293', 17, 'quos rerum exercitationem', 41);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Nesciunt rerum quia vitae amet dolorem.', '2019-06-02 21:00:12.127', 18, 'ut laudantium vero', 78);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('quisquam', '2019-06-02 17:18:45.444', 19, 'placeat et aspernatur', 79);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('voluptate maxime aliquam', '2019-06-02 14:16:38.261', 20, 'fugit harum aliquam', 81);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Soluta dolores sint libero tenetur molestiae.
+Et possimus culpa cupiditate beatae eum saepe voluptas sunt fugiat.
+Quia repudiandae velit autem harum asperiores sequi et.
+Voluptatum voluptate eaque in et ipsum consectetur.', '2019-06-02 14:33:03.790', 21, 'sunt est voluptate', 93);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Veniam iusto dolorum numquam in et vel reprehenderit. Omnis dolores placeat exercitationem culpa eligendi soluta. Fuga non temporibus possimus pariatur dolor consequatur voluptatem nemo ratione.
+
+Laboriosam explicabo autem vero ex placeat asperiores maxime sit. Et quo asperiores rerum ipsum. Dolorem velit ipsum repudiandae consequatur.
+
+Recusandae eaque corporis rerum mollitia et aliquid qui nisi. Magnam fugit eum reiciendis sunt praesentium blanditiis blanditiis. Quidem assumenda quis laborum nisi iusto est. A molestiae odit sunt id velit dolor mollitia. Neque quidem quia id quod nam. Vitae nihil eius commodi.', '2019-06-02 10:13:21.590', 22, 'sit eos et', 88);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Facilis accusamus ut quasi expedita rerum facilis numquam aut consectetur.', '2019-06-03 07:32:05.720', 23, 'maiores voluptate et', 29);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Perspiciatis voluptates modi consequatur sit. Vitae impedit quos quidem ut nemo dignissimos. Odio quo veritatis aut. Eos nobis voluptatem odit dolores aut nisi aut et molestias. Eos ipsum et quibusdam.', '2019-06-02 19:47:42.903', 24, 'ut et expedita', 72);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('deserunt', '2019-06-02 19:16:33.784', 25, 'iusto voluptatem reiciendis', 73);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Quae est facilis eius voluptas.', '2019-06-03 00:21:00.385', 26, 'reiciendis nesciunt minima', 20);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Harum neque cumque non quasi eos consectetur laborum. Veniam voluptas sed. Omnis minus quidem quam quia exercitationem et. Dolore ipsa vero iure eum.
+
+Omnis et quos consequatur. Quisquam quia quia exercitationem aliquid maxime ad qui illum. Laborum veritatis rerum nulla doloremque est optio voluptas. Atque et voluptate. Aut est est aperiam cum. Dolor qui ut officiis voluptatem voluptatem sunt corrupti molestias quod.
+
+Provident itaque ut omnis. Aspernatur libero veritatis quis quam repudiandae sunt omnis possimus. Et dolor ab temporibus sed delectus rerum. Sed repudiandae quibusdam ex. Ut ipsa dolore. Quis praesentium ea.', '2019-06-02 14:32:08.210', 27, 'nam numquam eligendi', 6);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Nemo necessitatibus et inventore dolores quo inventore.', '2019-06-03 00:31:27.676', 28, 'sapiente sit neque', 6);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Sapiente rem odit.
+Nobis beatae qui aspernatur autem est.', '2019-06-02 21:11:38.409', 29, 'aut excepturi deserunt', 96);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Labore eos qui blanditiis nulla quaerat repudiandae est dolore. Modi rerum deserunt. Adipisci animi culpa. Distinctio enim et officia quo. Aperiam ut culpa officiis aut quos. Nam perferendis qui.', '2019-06-02 22:47:01.298', 30, 'et odit sint', 65);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Quis distinctio et quaerat voluptate. Alias temporibus vitae assumenda in quod sunt animi illo.', '2019-06-02 22:55:52.205', 31, 'aut voluptatem cumque', 44);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Est totam exercitationem autem. Sed fugiat et velit molestias amet adipisci repudiandae provident. Unde nulla eos esse et libero.', '2019-06-03 03:31:42.897', 32, 'minima neque cumque', 74);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Voluptas hic nemo. Dolor et totam non autem sint. Blanditiis facilis molestias ea. Exercitationem quam harum et aut et. Minus asperiores omnis eligendi est cumque et facere aut fuga. Dolorum perspiciatis ut ullam adipisci ut ut modi et.', '2019-06-02 19:34:52.940', 33, 'veniam ad ut', 72);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('est doloribus officia', '2019-06-02 19:12:31.025', 34, 'nostrum voluptatem consequuntur', 92);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Non aut et ut et aut tempora ut voluptatem modi.', '2019-06-03 04:45:51.425', 35, 'inventore nesciunt maxime', 15);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('beatae', '2019-06-02 17:38:31.529', 36, 'accusantium quod optio', 25);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Corrupti aut enim qui eligendi.
+Maxime cumque ea error error fugit.
+Consequatur reprehenderit accusamus dolorem amet quis quo non dolor qui.', '2019-06-02 18:51:49.476', 37, 'similique qui quibusdam', 73);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Illo error minus deserunt et voluptates omnis id ducimus. Voluptatem perferendis quisquam quia ut placeat consequatur est eum ipsa. Cupiditate odit rerum dolore nesciunt autem vel ut consectetur. Expedita exercitationem corporis sed mollitia eum omnis temporibus provident.', '2019-06-03 01:04:46.985', 38, 'dolorem mollitia fugit', 55);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Consequatur omnis voluptates sequi repellat. Dolorem nam incidunt. Et aspernatur illo soluta ab.', '2019-06-03 08:23:29.819', 39, 'earum fugit perspiciatis', 53);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Assumenda blanditiis et sunt.', '2019-06-02 13:40:24.101', 40, 'quod quae voluptas', 69);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('sint officiis asperiores', '2019-06-02 14:28:18.499', 41, 'quia rem fugiat', 41);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Ad qui eligendi.', '2019-06-02 13:37:52.632', 42, 'repellendus voluptatum omnis', 18);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Reprehenderit non cum quia porro sed quasi rerum qui. Laborum voluptatibus possimus quod est molestiae doloremque fuga. Distinctio et facilis modi possimus et quibusdam. Vel quae est. Voluptas iste et eum voluptas. Quia id earum consequatur reprehenderit non modi labore.', '2019-06-02 21:52:19.274', 43, 'natus beatae fuga', 24);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Ea voluptates reprehenderit. Facere maxime nisi velit non odit quas. Sint numquam neque.
+
+Rerum sunt consequatur velit quidem recusandae numquam. Magni omnis fuga iusto excepturi nulla qui est magnam id. Totam non in dolorem. Et debitis accusantium quia vitae dignissimos. Dolor saepe quia assumenda. Dolorum non non consectetur quia est.
+
+Id incidunt omnis nobis nobis quae et aut. Autem doloremque quaerat ut sunt voluptatem minima error. Aperiam odit eveniet praesentium impedit aut.', '2019-06-02 09:58:56.818', 44, 'et quia impedit', 55);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Et voluptatum voluptates. Consectetur quaerat suscipit non delectus itaque facere omnis. Nobis est quae iure modi rerum qui enim. Aut quaerat consequatur est sit. Beatae vel dignissimos consequuntur et qui.', '2019-06-02 17:14:46.483', 45, 'eum sit quis', 43);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Ex corrupti sunt ut at inventore quo minima.
+Quia voluptates et dignissimos nihil.
+Sint et quo nemo aut impedit quisquam.
+Et et vel quia.
+Animi quaerat fugit et quis est.', '2019-06-02 19:23:18.986', 46, 'eum expedita perspiciatis', 67);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('dignissimos est labore', '2019-06-02 22:27:28.592', 47, 'officiis in quia', 55);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Cupiditate ut cumque.', '2019-06-03 00:54:09.839', 48, 'cupiditate commodi architecto', 85);
+insert into "topics" ("body", "created_at", "id", "title", "user_id") values ('Sed id velit soluta omnis repudiandae. Libero eos molestiae magnam qui ipsum occaecati sapiente quas. Deserunt sequi eum deleniti velit sit ut. Ipsam doloremque quos commodi quis soluta ut maiores odit. Molestiae et exercitationem quos blanditiis officia omnis veniam cumque.', '2019-06-02 23:47:54.586', 49, 'aut iure quibusdam', 90)
